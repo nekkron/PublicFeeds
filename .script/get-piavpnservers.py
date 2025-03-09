940cc40c709a801d509749a676ef065cbd3e07d1
James K <nekkron@gmail.com>
2025-03-09 15:29:19 +0100

    Update get-piavpnservers.py
    changed output folder structure

--- .script/get-piavpnservers.py
+++ .script/get-piavpnservers.py
@@ -96,7 +96,7 @@ def main():
     curr_path = Path.cwd()
 
     out_path = (
-        curr_path / "master" / "PublicFeeds" / "PIAVPNDaily" / "pia-servers.csv"
+        curr_path / "main" / "PublicFeeds" / "VPN" / "PIA" / "pia-servers-daily.csv"
     )
     try:
         out_path.parents[0].mkdir(parents=True, exist_ok=False)
